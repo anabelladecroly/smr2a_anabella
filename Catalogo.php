098f77a136126f44c3bc812e4d3e67cd4e222953
anabelladecroly <io2xxanabella@gmail.com>
2019-10-02 16:49:12 +0200

    Catalogo

---
+++ Catalogo.php
@@ -0,0 +1,26 @@
+<html>
+	<body>
+	<center>
+		<h1>LISTA DE PERFILES GITHUB</h1>
+		
+	</body>
+	<a href="https://github.com/dblancoformacion" target="_blank">Profesor David</a>
+	<br>
+	<a href="https://github.com/anabelladecroly" target="_blank">Anabella</a>
+	<br>
+	<a href="https://github.com/pablodecroly" target="_blank">Pablo</a>
+	<br>
+	<a href="https://github.com/gabrieldecroly" target="_blank">Gabriel</a>
+	<br>
+	<a href="https://github.com/Alberto-cloud" target="_blank">Alberto</a>
+	<br>
+	<a href="https://github.com/victorflorea" target="_blank">Victor.F</a>
+	<br>
+	<a href="https://github.com/VictorCaboRamirez" target="_blank">Victor.C</a>
+	<br>
+	<a href="https://github.com/Jhoss11" target="_blank">Jhostin</a>
+	
+
+	</center>
+</html>	
+
